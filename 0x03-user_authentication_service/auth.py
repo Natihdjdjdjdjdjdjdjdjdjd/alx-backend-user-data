b03a1c1a11f99b3d015d55dd580d5112bc0a06e1
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-06-11 18:56:05 +0000

    main5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,25 @@ def _hash_password(password: str) -> bytes:
     """
     encod_pwd = password.encode()
     return bcrypt.hashpw(encod_pwd, bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            # If a user already exist with the passed email
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        ha_pword = _hash_password(password)
+        # Save the user to the database using self._db
+        user = self._db.add_user(email, ha_pword)
+        # Return the User object
+        return user
